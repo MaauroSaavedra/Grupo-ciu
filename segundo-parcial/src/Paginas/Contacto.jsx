8630b1f7509cfd40cd1cb9dbfd635b4dc96091cd
Juanmabritez <juanmanuelbritez23@gmail.com>
2024-07-17 23:07:04 -0300

    arreglos en contacto y mapa

--- segundo-parcial/src/Paginas/Contacto.jsx
+++ segundo-parcial/src/Paginas/Contacto.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 const estiloLogo = {
-   height: '100%',
+   height: '50px',
    padding :'0 50%',
-   margin: '0 auto',
-   width: '15%'
+   margin: 'auto'
 }
 const estiloMapa = {
     border : '0',
     borderRadius : '10px',
-    width: '30%'
+    margin: '10px'
 }
 
 const estiloBotonA = {
@@ -21,51 +20,53 @@ const estiloConsulta = {
     width: '30%',
     height: '15%',
     backgroundColor: 'bisque',
+    margin: '10px'
 }
 const Contacto = () => {
     return (
-        <article>
+        <article className="flex-container">
 
             <section id="ubicacion">
                 <h3>Como llegar</h3>
-                <iframe id="direccion" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.5504789124307!2d-58.39096488780118!3d-34.59023917284639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccabb5bc50095%3A0xa6be1dce51dc1c5a!2sRodr%C3%ADguez%20Pe%C3%B1a%201766%2C%20C1021ABL%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1716128786028!5m2!1ses!2sar" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe id="direccion" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6564.213603594494!2d-58.630110472440705!3d-34.652005604885005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc7dbb6e8e933%3A0xed7cbdaf2eb10fa9!2sThe%20Coffee%20Store!5e0!3m2!1ses!2sar!4v1721263471534!5m2!1ses!2sar" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </section>
+        <div className="flex-container-center" id="tabla">
         <table id="informacionLugar">
             <br />
             <tr>                
-                <td>Telefono:</td>
-                <td>4450-3265</td>
+                <td className="letra">Telefono:</td>
+                <td className="letra">4450-3265</td>
             </tr>
             <br />
             <tr>                
-                <td>Correo Electronico:</td>
-                <td>elrincondelaroma@gmail.com</td>
+                <td className="letra">Correo Electronico:</td>
+                <td className="letra">elrincondelaroma@gmail.com</td>
             </tr>
             <br />
             <tr>                
-                <td>Direccion:</td>
-                <td>Rodríguez Peña 1766</td>
+                <td className="letra">Direccion:</td>
+                <td className="letra">Rodríguez Peña 1766</td>
             </tr>
             <br />
             <tr>                
-                <td>Horario Mañana:</td>
-                <td>Lunes a sabado de 9:00 a 14:00</td>
+                <td className="letra">Horario Mañana:</td>
+                <td className="letra">Lunes a sabado de 9:00 a 14:00</td>
             </tr>
             <br />
             <tr>                
-                <td>Horario Nocturno:</td>
-                <td>Lunes a sabado de 17:00 a 22:00</td>
+                <td className="letra">Horario Nocturno:</td>
+                <td className="letra">Lunes a sabado de 17:00 a 22:00</td>
             </tr>
             <br />
             <tr>                
-                <td>Instagram:</td>
-                <td><a href="https://www.instagram.com/cafemartinezoficial/">Cafeteria El Aroma</a></td>
+                <td className="letra">Instagram:</td>
+                <td><a id="cafeteria" href="https://www.instagram.com/cafemartinezoficial/">Cafeteria El Aroma</a></td>
             </tr>
             <br />
             <tr>
                 <td>
-                    <form>
-                        <label>Consultas:</label>
+                    <form className="letra">
+                        <label >Consultas:</label>
                         <div className="flex-container-column">
                             <textarea required style={estiloConsulta}></textarea>
                         <button type="submit" style={estiloBotonA} >Enviar</button>
@@ -75,9 +76,11 @@ const Contacto = () => {
                 </td>
             </tr>
             <tr>
-            <td><img style={estiloLogo} src="https://cdn-icons-png.flaticon.com/512/1114/1114350.png" alt="logo de instagram" /></td>
-            </tr>        
-        </table>
+            <td className="flex-container">
+                <img style={estiloLogo} src="https://cdn-icons-png.flaticon.com/512/1114/1114350.png" alt="logo de instagram" /></td>
+            </tr>
+            </table>
+        </div>
         
     </article>)
 }
